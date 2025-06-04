37e10c76205bfc20f97aac050d14c1065d4df193
IanTech23 <ianheroku4@gmail.com>
2025-06-04 05:34:20 +0300

    Update config.cjs
    Signed-off-by: IanTech23 <ianheroku4@gmail.com>

--- config.cjs
+++ config.cjs
@@ -19,7 +19,7 @@ const config = {
   
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : true, 
-  ANTI_WORD: process.env.ANTI_WORD !== undefined ? process.env.ANTI_WORD === 'true' : true,
+  ANTI_WORD: process.env.ANTI_WORD !== undefined ? process.env.ANTI_WORD === 'false' : true,
   ANTI_DELETE: process.env.ANTI_DELETE !== undefined ? process.env.ANTI_DELETE === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "Ian Tech",
